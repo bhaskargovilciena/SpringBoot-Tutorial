f2a6dfc669b6524c70bee011c3bdd0cfe051826a
bgovil <bgovil@ciena.com>
2026-01-28 15:33:12 +0530

    dependency injection using IOC container (part 1)

--- src/main/java/com/javatutorial/firstProject/FirstProjectApplication.java
+++ src/main/java/com/javatutorial/firstProject/FirstProjectApplication.java
@@ -8,6 +8,10 @@ public class FirstProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstProjectApplication.class, args);
+		// injecting the object without creating the object. we get the object from the IOC container which is of the type ApplicationContext
+
+		MyClass myClass;
+		myClass.sayHello();
 	}
 
 }
---
+++ src/main/java/dependencyinjection/DIAndIOC.java
@@ -0,0 +1,17 @@
+package dependencyinjection;
+
+public class DIAndIOC {
+    public static void main(String[] args) {
+        /*
+        dependency injection means that instead of we're creating every object using the new keyword, the spring framework gives the ability to create objects on its own and we can focus on business logic
+        2 types of injection -
+        1. setter injection - we pass the value / ref of the service in the setter function of the class and the class sets the object
+        2. constructor injection - we pass the value / ref of the service in the constructor of the class and the class sets the object
+
+        to run the code using the framework, we need to set the config of the web app
+        before we run the code, we need to install and config tomcat to run the web app in system
+
+
+         */
+    }
+}
